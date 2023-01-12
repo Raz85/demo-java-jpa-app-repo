b236e5af9aba1f02768cdcab02fb3dc584398ca7
Raz85 <53524300+Raz85@users.noreply.github.com>
2023-01-12 12:25:22 +0200

    Update Demo7JpaApplicationTests.java
    Add a comment on Demo7JpaApplicationTests class

--- demo-7-JPA/src/test/java/com/example/demo/Demo7JpaApplicationTests.java
+++ demo-7-JPA/src/test/java/com/example/demo/Demo7JpaApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+#TEST GIT
+
 @SpringBootTest
 class Demo7JpaApplicationTests {
 
